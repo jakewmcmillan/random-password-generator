0a9801e4fb93ae086f22028d8489dd34e7b25b7f
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-20 20:17:20 -0400

    Adding window prompts

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var generateBtn = document.querySelector('#generate');
 function generatePassword() {
   var password = 'password';
   // TODO: add code to generate the password here
+  var userChoice1 = window.prompt("Choose password length between 8 and 128 characters:");
+
+  var userChoice2 = window.prompt("Do you want your password to include lowercase letters?");
+
+  var userChoice3 = window.prompt("Do you want your password to include uppercase letters?");
+
+  var userChoice4 = window.prompt("Do you want your password to include numbers?");
+
+  var userChoice5 = window.prompt("Do you want your passwrod to include special characters?")
 
   return password;
 }
