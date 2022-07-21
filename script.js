80e516a9b97f78efb902321fbe2fc295da957266
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-21 16:06:54 -0400

    Reverting back to original code

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ function generatePassword() {
   var password = 'password';
   // TODO: add code to generate the password here
 
-  var userChoice1 = window.prompt("Choose password length between 8 and 128 characters:");
-
-  var userChoice2 = window.prompt("Do you want your password to include lowercase letters y/n?");
-  var userChoice3 = window.prompt("Do you want your password to include uppercase letters y/n?");
-  var userChoice4 = window.prompt("Do you want your password to include numbers y/n?");
-  var userChoice5 = window.prompt("Do you want your passwrod to include special characters y/n?");
-
-  var index = Math.floor(Math.random() * password.length);
-
   return password;
 }
 
